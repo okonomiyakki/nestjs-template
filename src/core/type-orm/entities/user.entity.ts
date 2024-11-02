7861d5b1986fdadf525e01f3161417a1be7d6686
okonomiyakki <parkji12532@naver.com>
2024-11-02 19:01:25 +0900

    feat: UserEntity 만들기

---
+++ src/core/type-orm/entities/user.entity.ts
@@ -0,0 +1,47 @@
+import { Column, CreateDateColumn, Entity, Index, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { TokenEntity } from '@core/type-orm/entities/token.entity';
+
+@Index('UQ_IDX_user_nickname', ['nickname'], { unique: true })
+@Index('UQ_IDX_user_email', ['email'], { unique: true })
+@Entity('user')
+export class UserEntity {
+  @PrimaryGeneratedColumn('uuid', {
+    name: 'id',
+    comment: 'User UUID (PK, NN)',
+  })
+  id!: string;
+
+  @Column('varchar', {
+    name: 'nickname',
+    length: 10,
+    nullable: false,
+    comment: 'User Nickname (NN, UQ)',
+  })
+  nickname!: string;
+
+  @Column('varchar', {
+    name: 'email',
+    length: 50,
+    nullable: false,
+    comment: 'User Email (NN, UQ)',
+  })
+  email!: string;
+
+  @Column('varchar', {
+    name: 'password',
+    length: 60,
+    nullable: false,
+    comment: 'User Password (NN)',
+  })
+  password!: string;
+
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'timestamp',
+    comment: 'User Creation Date (NN)',
+  })
+  createdAt!: Date;
+
+  @OneToOne(() => TokenEntity, (token) => token.user)
+  token!: TokenEntity;
+}
