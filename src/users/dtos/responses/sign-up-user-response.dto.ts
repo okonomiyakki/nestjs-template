6b09d2b6262d2a03878e6bda20442ce2ef2d8450
okonomiyakki <parkji12532@naver.com>
2024-11-02 23:25:26 +0900

    feat: 회원 가입 response dto에서 base dto extends하기

--- src/users/dtos/responses/sign-up-user-response.dto.ts
+++ src/users/dtos/responses/sign-up-user-response.dto.ts
@@ -1,16 +1,11 @@
+import { BaseDto } from '@common/dtos/base.dto';
 import { Exclude, Expose } from 'class-transformer';
 
 @Exclude()
-export class SignUpUserResponseDto {
-  @Expose()
-  readonly id!: string;
-
+export class SignUpUserResponseDto extends BaseDto {
   @Expose()
   readonly email!: string;
 
   @Expose()
   readonly nickname!: string;
-
-  @Expose()
-  readonly createdAt!: Date;
 }
