5d4c4a28a80e8f7b0bd70d5d17aeaa2b97fc2ffa
okonomiyakki <parkji12532@naver.com>
2024-11-02 20:13:51 +0900

    feat: users모듈 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoreModule } from '@core/core.module';
+import { UsersModule } from '@users/users.module';
 
 @Module({
-  imports: [CoreModule],
+  imports: [CoreModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
