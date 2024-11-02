0b08d228929544a30b6752cabb994f01a3e232cd
okonomiyakki <parkji12532@naver.com>
2024-11-02 20:17:07 +0900

    feat: users컨트롤러 생성

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { UsersController } from '@users/controllers/users.controller';
 
-@Module({})
+@Module({
+  controllers: [UsersController],
+})
 export class UsersModule {}
