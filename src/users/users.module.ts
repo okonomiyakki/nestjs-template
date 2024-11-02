5bf3ebaff8beed4b85e8dce94f9fe3e1f878e41c
okonomiyakki <parkji12532@naver.com>
2024-11-02 20:29:03 +0900

    feat: TypeOrmExModule로 users 레포지토리 동적 등록

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,11 @@
+import { UserRepository } from '@core/type-orm/repositories/user.repository';
+import { TypeOrmExModule } from '@core/type-orm/type-orm-ex.module';
 import { Module } from '@nestjs/common';
 import { UsersController } from '@users/controllers/users.controller';
 import { UsersService } from '@users/services/users.service';
 
 @Module({
+  imports: [TypeOrmExModule.forCustomRepository([UserRepository])],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
