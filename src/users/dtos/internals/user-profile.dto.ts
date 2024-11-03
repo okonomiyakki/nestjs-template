afde5981a955ff285ee0751cf0e4fdc1a7c29f73
okonomiyakki <parkji12532@naver.com>
2024-11-03 17:57:20 +0900

    feat: user profile dto 만들기
    - 특정 조건으로 유저를 조회하고, 조회 된 객체를 그대로 초기화하기 위한 dto 클래스

---
+++ src/users/dtos/internals/user-profile.dto.ts
@@ -0,0 +1,14 @@
+import { BaseDto } from '@common/dtos/base.dto';
+import { IUser } from '@users/interfaces/user.interface';
+import { Expose } from 'class-transformer';
+
+export class UserProfileDto extends BaseDto implements IUser {
+  @Expose()
+  readonly email!: string;
+
+  @Expose()
+  readonly nickname!: string;
+
+  @Expose()
+  readonly password!: string;
+}
