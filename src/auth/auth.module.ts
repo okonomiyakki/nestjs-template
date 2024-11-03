0fc5f372a7214c40bf9d1ec95e83a41d0c8bf78d
okonomiyakki <parkji12532@naver.com>
2024-11-03 11:24:24 +0900

    feat: auth 모듈에서 users 모듈 사용할거임

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from '@auth/controllers/auth.controller';
 import { AuthService } from '@auth/services/auth.service';
+import { UsersModule } from '@users/users.module';
 
 @Module({
+  imports: [UsersModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
