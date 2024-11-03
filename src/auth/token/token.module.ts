f7c5cfde4db4ba7b6d7eaacf54bae94b33bd23b5
okonomiyakki <parkji12532@naver.com>
2024-11-03 19:51:29 +0900

    feat: TypeOrmExModule로 token 레포지토리 동적 등록

--- src/auth/token/token.module.ts
+++ src/auth/token/token.module.ts
@@ -1,7 +1,10 @@
+import { TokenRepository } from '@core/type-orm/repositories/token.repository';
+import { TypeOrmExModule } from '@core/type-orm/type-orm-ex.module';
 import { Module } from '@nestjs/common';
 import { TokenService } from '@token/services/token.service';
 
 @Module({
+  imports: [TypeOrmExModule.forCustomRepository([TokenRepository])],
   providers: [TokenService],
   exports: [TokenService],
 })
