75b630dbc170dcf40081a4c44c5075687fc49a68
okonomiyakki <parkji12532@naver.com>
2024-11-03 18:33:48 +0900

    feat: 로그인 API에서 사용자 인증까지만 구현함
    - token 모듈 만들고, jwt 만드는 비지니스 로직 만들어야됨
    - 응답 타입 any는 token dto 만들고나서 바꾸기

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { SignInRequestDto } from '@auth/dtos/requests/sign-in-request.dto';
+import { AuthService } from '@auth/services/auth.service';
+import { Body, Controller } from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  async signIn(@Body() signInRequest: SignInRequestDto): Promise<any> {
+    return await this.authService.signInUser(signInRequest);
+  }
+}
--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,4 +1,17 @@
+import { SignInRequestDto } from '@auth/dtos/requests/sign-in-request.dto';
 import { Injectable } from '@nestjs/common';
+import { UserProfileResponseDto } from '@users/dtos/responses/user-profile.response.dto';
+import { UsersService } from '@users/services/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly usersService: UsersService) {}
+
+  async signInUser(signInRequest: SignInRequestDto): Promise<any> {
+    const { email, password } = signInRequest;
+
+    const user: UserProfileResponseDto = await this.usersService.verifyUser(email, password);
+
+    // TODO: JWT 발급 로직 구현
+  }
+}
