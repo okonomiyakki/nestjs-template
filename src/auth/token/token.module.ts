5b0cdb5fa63c3657ff4aebcf6318bf10e5f6cda8
okonomiyakki <parkji12532@naver.com>
2024-11-04 16:33:25 +0900

    fix: JwtModuleOptions factory파일 token 모듈 하위로 옮기기

--- src/auth/token/token.module.ts
+++ src/auth/token/token.module.ts
@@ -1,8 +1,8 @@
-import { JwtModuleOptionsFactory } from '@auth/jwt/factories/jwt-module-options.factory';
 import { TokenRepository } from '@core/type-orm/repositories/token.repository';
 import { TypeOrmExModule } from '@core/type-orm/type-orm-ex.module';
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtModuleOptionsFactory } from '@token/jwt/factories/jwt-module-options.factory';
 import { TokenService } from '@token/services/token.service';
 
 @Module({
