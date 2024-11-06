f735b1feab02490b15c790784a85430eaf0bd672
okonomiyakki <parkji12532@naver.com>
2024-11-06 21:51:38 +0900

    feat: Swagger 셋업
    - 헤더 Bearer 기반 액세스 토큰 인증 구성
    - 쿠키 기반 리프레시 토큰 인증 구성
    - 인증 정보(헤더, 쿠키) 새로고침 후에도 유지 기능 활성화
    - API 태그를 알파벳 순으로 정렬하고 HTTP 메서드 순서에대로 정렬하기

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { SERVER_CONFIG_TOKEN } from '@core/config/constants/config.token';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from '@src/app.module';
 import cookieParser from 'cookie-parser';
 
@@ -23,6 +24,55 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Template')
+    .setDescription(
+      'This template helps you quickly start your user management service in Docker environments.',
+    )
+    .setVersion('0.0.1')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        in: 'header',
+        description: 'Access Token required for authentication',
+      },
+      'access-token',
+    )
+    .addCookieAuth(
+      'refreshToken',
+      {
+        type: 'apiKey',
+        in: 'cookie',
+        description: 'Refresh Token for renewing access token',
+      },
+      'refresh-token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+      tagsSorter: 'alpha',
+      operationsSorter: (a: Map<any, any>, b: Map<any, any>) => {
+        const order = {
+          post: '0',
+          get: '1',
+          put: '2',
+          patch: '3',
+          delete: '4',
+        } as const;
+
+        return order[a.get('method') as keyof typeof order].localeCompare(
+          order[b.get('method') as keyof typeof order],
+        );
+      },
+    },
+  });
 
   await app.listen(PORT);
 }
