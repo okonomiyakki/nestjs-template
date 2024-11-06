9b9dad1d4ed6694f0e1a3192f89ced586685fbf0
okonomiyakki <parkji12532@naver.com>
2024-11-06 21:07:46 +0900

    feat: API 엔트포인트 'api'로 시작

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api');
+
   app.use(cookieParser());
 
   app.useGlobalPipes(
