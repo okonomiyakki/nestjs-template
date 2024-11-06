7d38de03899d36fa2dd126f5cc98d9e83c71bc9f
okonomiyakki <parkji12532@naver.com>
2024-11-06 22:05:34 +0900

    feat: request dto swagger 적용

--- src/auth/dtos/requests/sign-in-request.dto.ts
+++ src/auth/dtos/requests/sign-in-request.dto.ts
@@ -1,12 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { USER_EMAIL_LENGTH, USER_PASSWORD_REGEXP } from '@users/constants/user.constant';
 import { IsEmail, IsNotEmpty, IsString, Length, Matches } from 'class-validator';
 
 export class SignInRequestDto {
+  @ApiProperty({
+    description: 'User Email',
+    type: String,
+    example: 'example@eamil.com',
+  })
   @IsNotEmpty()
   @IsEmail()
   @Length(USER_EMAIL_LENGTH.MIN, USER_EMAIL_LENGTH.MAX)
   readonly email!: string;
 
+  @ApiProperty({
+    description: 'User Password',
+    type: String,
+    example: 'P@ssw0rd4321',
+  })
   @IsNotEmpty()
   @IsString()
   @Matches(USER_PASSWORD_REGEXP)
--- src/users/dtos/requests/sign-up-request.dto.ts
+++ src/users/dtos/requests/sign-up-request.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   USER_EMAIL_LENGTH,
   USER_NICKNAME_LENGTH,
@@ -6,16 +7,31 @@ import {
 import { IsEmail, IsNotEmpty, IsString, Length, Matches } from 'class-validator';
 
 export class SignUpRequestDto {
+  @ApiProperty({
+    description: 'User Email',
+    type: String,
+    example: 'example@eamil.com',
+  })
   @IsNotEmpty()
   @IsEmail()
   @Length(USER_EMAIL_LENGTH.MIN, USER_EMAIL_LENGTH.MAX)
   readonly email!: string;
 
+  @ApiProperty({
+    description: 'User Password',
+    type: String,
+    example: 'examplepw123*',
+  })
   @IsNotEmpty()
   @IsString()
   @Matches(USER_PASSWORD_REGEXP)
   readonly password!: string;
 
+  @ApiProperty({
+    description: 'User Nickname',
+    type: String,
+    example: 'example',
+  })
   @IsNotEmpty()
   @IsString()
   @Length(USER_NICKNAME_LENGTH.MIN, USER_NICKNAME_LENGTH.MAX)
