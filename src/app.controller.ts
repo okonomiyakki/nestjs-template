effe7f7f017f8dbab72fe1c021165a843e001cc3
okonomiyakki <parkji12532@naver.com>
2024-11-06 22:32:46 +0900

    feat: 이 템플릿 에서는 Swagger 핸들러임

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { AppService } from '@src/app.service';
 
+@ApiTags('API-Docs Swagger')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
