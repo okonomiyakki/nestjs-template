e085b545726cef60c8c60df56676413bb63ad71b
okonomiyakki <parkji12532@naver.com>
2024-11-06 21:23:40 +0900

    feat: 서버 실행 포트 동적 할당

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
+import { SERVER_CONFIG_TOKEN } from '@core/config/constants/config.token';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@src/app.module';
 import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+
+  const PORT = configService.get(SERVER_CONFIG_TOKEN).server.port;
 
   app.setGlobalPrefix('api');
 
@@ -18,6 +24,6 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
