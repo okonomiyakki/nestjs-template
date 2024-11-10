6317a8d8f87f6215c9ffd0aaac3fb20829a7c927
okonomiyakki <parkji12532@naver.com>
2024-11-10 14:38:25 +0900

    chore: 다듬기

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,22 +1,18 @@
 #!/bin/sh
 
-# Run migrations
 if [ "$MIGRATION_MODE" = "run" ]; then
   echo ">>> Running migrations..."
   npx ts-node ./node_modules/typeorm/cli.js -d ./typeorm.config.ts migration:run
 
-# Revert migrations
 elif [ "$MIGRATION_MODE" = "revert" ]; then
   echo ">>> Reverting migrations..."
   npx ts-node ./node_modules/typeorm/cli.js -d ./typeorm.config.ts migration:revert
 
-# Exit the container
 else
   echo ">>> No valid argument provided."
-  echo ">>> Please set the MIGRATION_MODE value to either 'run' or 'revert' in the dotenv file."
+  echo ">>> Please set the 'MIGRATION_MODE' value to either 'run' or 'revert' in the dotenv file."
   echo ">>> Exiting..."
   exit 1
 fi
 
-# Run the application
 node dist/main
